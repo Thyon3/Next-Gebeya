d32e2745a158c1cb0fd3fb1c9bad9db1abe28a42
Thyon3 <asnakemengesha80@gmail.com>
2026-06-07 20:39:03 -0400

    fix: size for the product item

--- pages/index.js
+++ pages/index.js
@@ -117,7 +117,7 @@ export default function Home({ featuredProducts = [], products = [], brands = []
           {isLoading ? (
             <SkeletonProductGrid count={siteSettings.latestProductsCount} />
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
               {products.map((product) => (
                 <ProductItem
                   product={product}
